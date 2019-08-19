cd942fd0bc7b9246453b977b34c9e6cd9eb8e335
David Ferrara <davidferrara@live.com>
2019-08-19 12:42:24 -0400

    updated exponentia

--- Easy/exponentia/exponentia.js
+++ Easy/exponentia/exponentia.js
@@ -1,42 +1,39 @@
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
+/*
+Problem statement:
+Create a code that allows people to learn about exponents.
+The task is to write a code which takes an integer number as an input and returns the values ranging from that of 2(two) raised to power 0(zero) to 2(two) raised to power n(the number specified in input)
 
-// declare global variables
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
+Input:
+Input contains the number 'n' for which all the values are returned from power of 2 from 0 to n.
 
-// standard input is stored into input_stdin
-process.stdin.on('data', function (data) {
-  input_stdin += data;
-});
+Output:
+Output contains the values ranging from 2(two) raised to power 0(zero) to 2(two) raised to power n(input) separated by a comma.
+Note: For testcases with negative number as input, 2 raise to power 0 will be considered as 1.0 instead of 1 whereas for other negative powers the trailing zeros in output will be truncated like 0.50 is 0.5 , 0.25 is 0.25, 0.1250 is 0.125
 
-// standard input is done and stored into an array
-// then main is called so that you can start processing your data
-process.stdin.on('end', function () {
-  input_stdin_array = input_stdin.split("\n");
-  main();
-});
+Constraint:
+-1000 ≤ n ≤ 1000
 
-// reads a line from the standard input array
-function readLine() {
-  return input_stdin_array[input_currentline++];
-}
+Sample Input:
+5
+
+Sample Output:
+1,2,4,8,16,32
+*/
 
 function main() {
-  var n = parseInt(readLine());
-  var answer = [];
-  
-  if(n >= 0) {
-  	for(let i = 0; i <= n; i++) {
-  		answer.push(Math.pow(2, i));
-  	}
-  } else {
-  	var x = 1;
-  	answer.push(x.toFixed(1));
-  	for(let i = -1; i >= n; i--) {
-  		answer.push(Math.pow(2, i));
-  	}
-  }
-  console.log(answer.toString());
+    var n = parseInt(readLine());
+    var answer = [];
+
+    if (n >= 0) {
+        for (let i = 0; i <= n; i++) {
+            answer.push(Math.pow(2, i));
+        }
+    } else {
+        var x = 1;
+        answer.push(x.toFixed(1));
+        for (let i = -1; i >= n; i--) {
+            answer.push(Math.pow(2, i));
+        }
+    }
+    console.log(answer.toString());
 }
